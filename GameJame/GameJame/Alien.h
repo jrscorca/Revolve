2930c5b7bb357b534fc2cd6d596b6b2f94e5d215
Joshua <jrscorca@gmail.com>
2014-04-26 11:11:01 -0700

    +new files

---
+++ GameJame/GameJame/Alien.h
@@ -0,0 +1,13 @@
+//
+//  Alien.h
+//  GameJame
+//
+//  Created by Joshua Scorca on 4/26/14.
+//  Copyright (c) 2014 Joshua Scorca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Alien : NSNode
+
+@end
---
+++ GameJame/GameJame/Alien.m
@@ -0,0 +1,13 @@
+//
+//  Alien.m
+//  GameJame
+//
+//  Created by Joshua Scorca on 4/26/14.
+//  Copyright (c) 2014 Joshua Scorca. All rights reserved.
+//
+
+#import "Alien.h"
+
+@implementation Alien
+
+@end
